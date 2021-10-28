f673778746b37282085c1dd4e64d2c71fec9419b
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2021-10-28 21:36:23 +0600

    upload

---
+++ INSTALL ME FIRST/sql for database/job.sql
@@ -0,0 +1,14 @@
+INSERT INTO `addon_account` (name, label, shared) VALUES
+	('society_marijuana', 'Marijuana Farm', 1)
+;
+
+INSERT INTO `jobs` (name, label) VALUES
+	('marijuana', 'Marijuana Farm')
+;
+
+INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
+	('marijuana',0,'farmer','Farmer',20,'{}','{}'),
+	('marijuana',1,'shopkeeper','Shopkeeper',40,'{}','{}'),
+	('marijuana',2,'manager','Manager',60,'{}','{}'),
+	('marijuana',3,'boss','Owner',100,'{}','{}')
+;
---
+++ config-shop.lua
@@ -0,0 +1,27 @@
+Config.ShopMenu = vector3(-194.27, -1715.29, 27.31)    --Shop menu for employee
+
+Config.Shops = {
+    Shop1 = {
+        Pos = vector3(-187.99, -1703.82, 32.03),     --Shop menu for customer
+    },
+}
+
+Config.ShopAlowedItems = {                          --Allowed item list that employees can put in the shop for sell
+    [1] =   'backwoods_grape',
+    [2] =   'backwoods_honey',
+    [3] =   'cake_mix',
+    [4] =   'cereal_milk',
+    [5] =   'cheetah_piss',
+    [6] =   'gary_payton',
+    [7] =   'gelatti',
+    [8] =   'georgia_pie',
+    [9] =   'jefe',
+    [10] =  'white_runtz',
+    [11] =  'whitecherry_gelato',
+    [12] =  'blueberry_cruffin',
+    [13] =  'snow_man',
+    [14] =  'lighter',
+    [15] =  'backwoods_russian_cream',
+    [16] =  'cheap_lighter',
+    [17] =  'grabba_leaf',
+}
